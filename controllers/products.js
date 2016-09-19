7482cd3cfc799d2743640885f1b94fa03e18ef19
SharksevenRo <937257166@qq.com>
2016-09-19 17:17:04 +0800

    add company model

---
+++ controllers/products.js
@@ -0,0 +1,5 @@
+var util = require('../utils.js');
+var users = require('./common');
+
+
+module.exports = users;
---
+++ models/productModel.js
@@ -0,0 +1,28 @@
+var Sequelize = require("sequelize");
+
+module.exports = function(sequelize, DataTypes){
+    const product = sequelize.define("gospel_products", {
+			id: {
+				type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4 ,
+				primaryKey: true
+			},
+	    name: DataTypes.STRING,
+      price: DataTypes.DOUBLE,
+      defaultDisk: { type: DataTypes.DOUBLE, field: "default_disk", defaultValue: 0 },
+      parent: DataTypes.STRING,
+      peopleLimit: { type: DataTypes.INTEGER, field: "people_limit", defaultValue: 0 },
+      format: DataTypes.STRING,
+      isDeletted: { type: DataTypes.INTEGER, field: "isdeleted", defaultValue: 0 }
+	  },{
+			timestamps: true,
+      createdAt: 'createat',
+      updatedAt: 'updateat',
+      classMethods:{
+          associate: (models) => {
+                      console.log("associate");
+                  }
+      }
+    });
+    return product;
+}
