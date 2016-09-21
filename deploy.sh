a54628fe84967c9e08cb9875fd19dfef77e1d3d8
leinue <597055914@qq.com>
2016-09-21 10:12:55 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ git add .
 git commit -a -m "$msg"
 git push -u origin master
 #sync
-ssh root@poimoe.com 'cd /var/www/gospely/api && git pull'
+ssh root@119.29.243.71 'cd /var/www/gospely/api && git pull'
