22439254610b247d6324cda247a27f1c29b4a0d8
SharksevenRo <937257166@qq.com>
2016-10-12 14:33:54 +0800

    update

--- server/auth/config.js
+++ server/auth/config.js
@@ -2,7 +2,7 @@ module.exports = {
   weixin: {
     appid: 123,
     appsecret: "xxxxxxxxx",
-    redirect_url: 'http://www.dodora.cn/weixin/login/callback',
+    redirect_url: 'http://api.dodora.cn/weixin/login/callback',
     response_type: 'code',
     scope: 'snsapi_login'
   }
