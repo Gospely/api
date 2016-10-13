4c77c4df54992983aaf87ab54723c23f38790058
SharksevenRo <937257166@qq.com>
2016-10-13 16:34:41 +0800

    update weixinLogin resgister

--- server/auth/wechat.js
+++ server/auth/wechat.js
@@ -1,10 +1,6 @@
-/**
- *
- * Created by lilun on 14-10-19.
- *
- * 这个模块用于向用户请求微信授权后与微信平台进行code、accessToken交互及获取的模块。
- */
 var request = require('co-request');
+var models =require('../../models');
+
 module.exports = getWechatAuths;
 
 function getWechatAuths(appid_, secret_) {
@@ -29,8 +25,31 @@ function getWechatAuths(appid_, secret_) {
         console.log("openid: " + res.openid);
         var userBase = yield getInfo(res.access_token, res.openid);
         if (userBase = null)return this.status = 400;
-        this.status = 200;
-        yield next;
+
+        //获取到了用户的详细信息
+
+        var userBase = {"openid":"ouvLcwUfaoXgmY_vr2Xa4a4YDn68","nickname":"Sharkseven","sex":1,"language":"zh_CN","city":"Nanchang","province":"Jiangxi","country":"CN","headimgurl":"http:\/\/wx.qlogo.cn\/mmopen\/Q3auHgzwzM5ibkUVWRiafgndMU46DVwY4wCoGHskX2HIoicFcKwfKxWU8DZM7Kc2bOqy0auNsowPgErHYD4pJdibYA\/0","privilege":[],"unionid":"o4y4CvyGj6qXeOsLSWbKawN2YEyQ"};
+
+        var data = yield models.gospel_users.getAll(
+           {
+            openId: userBase['unionid']
+          }
+        );
+        if(data.length == 0){
+
+            //完善信息
+            var isInsert = yield models.gospel_users.create({
+              name:userBase['nickname'],
+              photo:userBase['headimgurl'],
+              openId:userBase['unionid'],
+              phone:'110',
+              password:'123'
+            });
+            this.redirect("http://www.baidu.com?openId= " + userBase['unionid']);
+        }else{
+          //设置登录
+          this.redirect("http://localhost:8088");
+        }
     }
 }
 
