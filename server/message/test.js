321aa41f3f4cb2fa575bbb68b7b2fd9183ae6ca6
SharksevenRo <937257166@qq.com>
2016-10-17 18:15:08 +0800

    update

--- server/message/test.js
+++ server/message/test.js
@@ -3,7 +3,7 @@ var message = require('./message');
 var options = {
 
     mobile:13456887748,
-    msg: '你好',
+    msg: '【江西龙猫科技】123456，是您的短信验证码，请在60秒内提交验证码',
     needstatus: false
 }
 message(options)
