43e20ff186b060225c3e23f0b46d882150ba2614
leinue <597055914@qq.com>
2016-10-18 15:12:52 +0800

    update

--- controllers/fs.js
+++ controllers/fs.js
@@ -253,9 +253,9 @@ var fileSystem = {
 
 			};
 
-			this.body = util.resp(200, '读取文件夹成功' + os.platform(), result);
+			this.body = util.resp(200, '读取文件夹成功' + os.platform() + os.platform == 'darwin', result);
 		}catch(err) {
-			this.body = util.resp(500, '读取文件夹失败' + os.platform(), err.toString());
+			this.body = util.resp(500, '读取文件夹失败' + os.platform() + os.platform == 'darwin', err.toString());
 		}
 
 	}
