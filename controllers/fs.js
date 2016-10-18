97d2622c3e7a2c0e90b817f908e3aad43aa0805c
leinue <597055914@qq.com>
2016-10-18 09:50:30 +0800

    update

--- controllers/fs.js
+++ controllers/fs.js
@@ -1,19 +1,38 @@
 var util = require('../utils.js'),
 	fs = require('fs'),
-	path = require('path');
+	path = require('path'),
+	co = require('co');
 
 var fileSystem = {};
 
-fileSystem.baseDir = '/var/www/storage/codes/vue-f7/';
+var config = {
+	baseDir: '/var/www/storage/codes/vue-f7/'
+};
 
-fileSystem.read =  function* (){
+process.platform === 'darwin' ? 
+	config.baseDir = '/var/www/apache/gospel/vue-f7' : 
+	config.baseDir = '/var/www/storage/codes/vue-f7'
 
-	// fs.readFile();
+fileSystem.read = function* (){
 
-	console.log("tests");
-	// this.body = 'fuck';
-	var resp = util.initResp(this);
-	resp(100, 'fuck', fileSystem);
+	var self = this;
+
+	var readFile = function (fileName){
+	  return new Promise(function (resolve, reject){
+	    fs.readFile(fileName, {flag: 'r+', encoding: 'utf8'}, function(error, data){
+	      if (error) reject(error);
+	      resolve(data);
+	    });
+	  });
+	};
+
+	try {
+		var fileContent = yield readFile(config.baseDir + '/index.html');
+		self.body = util.resp(200, '读取成功', fileContent.toString());
+	}catch(err) {
+		self.body = util.resp(500, '读取失败', err);
+	}
+
 
 }
 module.exports = fileSystem;
