40b425606bdf1b835a289ebbb6fc473208d89c74
SharksevenRo <937257166@qq.com>
2016-11-02 16:28:06 +0800

    update

--- process/index.js
+++ process/index.js
@@ -1,39 +1,84 @@
-var build = require("./node");
+module.exports = {
+	buildNext: function(node,options) {
 
-var node = build.init({
-	do: function() {
-		console.log("first do");
+		var temp = node;
+		while (null != temp.next) {
+				temp = temp.next;
+		}
+
+		temp.next = {
+			data: options.data,
+			excutable: true,
+			last: temp,
+			next: null,
+			excute: function(){
+
+				var self = this;
+				if(self.excutable) {
+						console.log(self.data);
+						self.excutable = false;
+						try {
+								options.do();
+								if(self.next != null){
+									self.next.excute();
+								}else {
+									console.log("finish");
+								}
+						} catch (e) {
+							self.rollback();
+						}
+
+				}else{
+					if(self.last != null){
+							self.last.excute();
+					}else{
+						console.log("back to begin");
+					}
+				}
+
+			},
+			rollback: function() {
+				var self = this;
+				if(self.last != null){
+					options.undo();
+					console.log("test");
+					self.last.rollback();
+				}else{
+					self.rollback();
+				}
+
+			}
+		};
+		return node;
 	},
-	data: 'first data',
-	undo: function() {
-		console.log("undo first");
-	},
-})
-node = build.buildNext(node, {
-	do: function() {
-			console.log("second do");
-		},
-	data: 'second data',
-	undo: function() {
-		console.log("undo second");
-	},
-});
-node = build.buildNext(node, {
-	do: function() {
-			console.log("third do");
-		},
-	data: 'third data',
-	undo: function() {
-		console.log("undo third");
-	},
-});
-node = build.buildNext(node, {
-	do: function() {
-			console.log("fourth do");
-		},
-	data: 'third data',
-	undo: function() {
-		console.log("undo fourth");
-	},
-});
-node.excute();
+	init: function(options) {
+
+		return {
+			data: options.data,
+			excutable: true,
+			last: null,
+			next: null,
+			excute: function(){
+				var self = this;
+
+				if(self.excutable) {
+						console.log(self.data);
+						self.excutable = false;
+						options.do();
+						self.next.excute();
+				}else{
+					self.rollback();
+					if(self.last != null){
+							self.last.excute();
+					}else{
+						console.log("stop");
+					}
+				}
+
+			},
+			rollback: function() {
+					console.log('rollback finish');
+			}
+		};
+	}
+}
