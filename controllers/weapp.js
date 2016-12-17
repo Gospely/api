48b4fa6232b15abae61379c283c725a40f035647
leinue <597055914@qq.com>
2016-12-17 14:33:30 +0800

    update

--- controllers/weapp.js
+++ controllers/weapp.js
@@ -114,9 +114,7 @@ var weapp = {
 
 			var dir = randomDir.split('/');
 			dir.pop();
-			dir.join('/');
-
-			dir.replace(',', '/');
+			dir = dir.join('/');
 
 			yield zip(dir);
 			rmdir(randomDir);
