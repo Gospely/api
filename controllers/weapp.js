3f738ad3811dc6c31c254a7d5b3e12aed4962dda
leinue <597055914@qq.com>
2016-12-17 13:27:46 +0800

    udate

--- controllers/weapp.js
+++ controllers/weapp.js
@@ -5,6 +5,7 @@ var util = require('../utils.js'),
 	parse = require('co-body'),
 	url = require('url');
 
+var send = require('koa-send');
 var os = require('os');
 var exec = require('child_process').exec;
 var dir = require('node-dir');
@@ -12,10 +13,84 @@ var multer = require('koa-multer');
 var path = require('path');
 var shells = require('../shell/index');
 
+var	writeFile = function(fileName, content) {
+		return new Promise(function(resolve, reject) {
+			fs.writeFile(fileName, content, function(error) {
+				if (error) {
+					reject(error);
+				};
+				resolve();
+			});
+		});
+	},
+
+	mkdir = function(fileName) {
+		return new Promise(function(resolve, reject) {
+			exec('mkdir ' + fileName, function(error, data) {
+				if (error) reject(error);
+				resolve(data);
+			});
+		});
+	},
+
+	rmdir = function(dir) {
+		return new Promise(function(resolve, reject) {
+			exec('rm -rf ' + dir, function(error, data) {
+				if (error) reject(error);
+				resolve(data);
+			});
+		});
+	},
+
 var weapp = {
 	pack: function*() {
 
-		this.body = util.resp(200, '访问成功', '云打包');
+		var app = yield parse(this);
+
+		if(typeof app == 'string') {
+			app = JSON.parse(app);
+		}
+
+		var randomDir = __dirname + randomString(8, 10);
+
+		var loopPack = function(dir) {
+
+			try {
+				mkdir(dir);
+
+				for(var key in app) {
+					var val = app[key],
+						filePath = '',
+					try {
+
+						if(typeof val == 'string') {
+
+							try {
+								filePath = dir + key;
+								yield writeFile(filePath, val);
+							}catch (err) {
+								rmdir(dir);
+								this.body = util.resp(500, '云打包失败', '创建文件: ' + key + '失败：' + err.toString());
+							}
+						}else {
+
+						}
+
+					} catch (err) {
+						rmdir(dir);
+						this.body = util.resp(500, '云打包失败', '创建文件夹失败：' + err.toString());
+					}
+
+				}
+
+			}catch (err) {
+				this.body = util.resp(500, '云打包失败', '创建项目主文件夹失败：' + err.toString());
+			}
+
+		}
+
+		loopPack(randomDir);
+
 	}
 }
 
--- utils.js
+++ utils.js
@@ -18,7 +18,29 @@ module.exports = {
 		}
 
 		return res;
+	},
+
+	randomString: function(randomFlag, min, max) {
+		
+	    min = min || 8;
+	    max = max || 10;
+	    randomFlag = randomFlag || false;
+
+	    var str = "",
+	        range = min,
+	        pos = '',        
+	        arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+	 
+	    // 随机产生
+	    if(randomFlag){
+	        range = Math.round(Math.random() * (max-min)) + min;
+	    }
+	    for(var i=0; i<range; i++){
+	        pos = Math.round(Math.random() * (arr.length-1));
+	        str += arr[pos];
+	    }
+
+	    return str;
 
 	}
-
 }
