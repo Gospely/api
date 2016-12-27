74339437fa64661d77c0ef8a095ccb3ddd30b430
SharksevenRo <937257166@qq.com>
2016-12-27 16:04:15 +0800

    update

--- container/index.js
+++ container/index.js
@@ -32,7 +32,7 @@ containers.filter = function*(next){
     var application = yield models.gospel_applications.findById(id);
     console.log(application.docker);
     this.containerName = application.docker;
-    this.ip = application.host;
+    this.remoteIp = application.host;
     console.log(application.host+"==============================");
     yield next;
 
