83ef0b9995a975aa35b707b747044539dbe705bc
SharksevenRo <937257166@qq.com>
2016-12-28 11:06:59 +0800

    update

--- shell/index.js
+++ shell/index.js
@@ -499,8 +499,7 @@ shells.initFrameWork = function() {
 shells.sshKey = function*(options){
      var host = options.host || '120.76.235.234';
      return new Promise(function(resolve, reject) {
-         exec('ssh root@' + host + '  docker exec gospel_project_' + options.docker +
-             ' ssh-keygen -t rsa -P "" -f ~/.ssh/id_rsa' ,
+         exec('ssh root@' + host + ' sh /root/gospely/deploy/shell/docker_bash.sh gospel_project_' + options.docker + " 'ssh-keygen -t rsa -P "" -f ~/.ssh/id_rsa'" ,
              function(err, data) {
                  if (err)
                      reject(err);
