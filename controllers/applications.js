ae667888757ee32250393cc3c40ace24e7e59b32
SharksevenRo <937257166@qq.com>
2017-01-05 09:08:37 +0800

    udate

--- controllers/applications.js
+++ controllers/applications.js
@@ -8,6 +8,7 @@ var processes = require('../process');
 var dnspod = require('../server/dnspod');
 var _md5 = require('../utils/MD5');
 var shell = require('../shell');
+var pay = require('../server/pay');
 var validator = require('../utils/validator');
 
 var applications = {};
@@ -52,11 +53,35 @@ applications.fast_deploy = function*(application,ctx){
 			 yield models.gospel_applications.delete(inserted.id);
 		}
 	} else {
-
+		var orderNo = _md5.md5Sign("gospel", uuid.v4());
+		var pay_url = yield pay.wechat.wxpay_pay({
+		  body: 'Gospel services', //商品描述,
+		  out_trade_no: orderNo, //商户订单号,
+		  total_fee: application.price, //金额,
+		//   total_fee: order.price * 100, //金额,
+		  spbill_create_ip: '127.0.0.1', //终端IP,
+		  product_id: '', //商品id 选,
+		  detail: 'dodora', //商品详情 选
+		  attach: '', //附加数据 选
+		  time_start: '', //订单生成时间 yyyyMMddHHmmss  选
+		  time_expire: '', //订单失效时间，格式为yyyyMMddHHmmss  选
+		  fee_type: '', //货币类型 选
+		}, ctx);
+		var url = pay.alipay.create_direct_pay_by_user({
+		  out_trade_no: orderNo //商户订单号, 商户网站订单系统中唯一订单号，必填
+		  ,
+		  subject: 'Gospel services' //订单名称 必填
+		  ,
+		  total_fee: 0.01 //付款金额,必填
+		  ,
+		  body: "dodora" //订单描述
+		  ,
+		  show_url: '1' //商品展示地址 需以http://开头的完整路径，例如：http://www.xxx.com/myorder.html
+		}, ctx);
 		application.id = uuid.v4();
 		var order = yield models.gospel_orders.create({
 			products: application.products,
-			orderNo: _md5.md5Sign("gospel", uuid.v4()),
+			orderNo: orderNo,
 			name: "付费Docker",
 			price: application.price,
 			status: 1,
@@ -65,12 +90,13 @@ applications.fast_deploy = function*(application,ctx){
 			timeUnit: application.unit,
 			unitPrice: application.unitPrice,
 			creator: application.creator,
-			application: application.id
+			application: application.id,
+			alipay: url,
+			wechat: pay_url.code_url
 		});
+
 		application.orderNo = order.id;
 		application.payStatus = -1;
-
-
 		delete application['products'];
 		delete application['price'];
 		delete application['size'];
@@ -104,10 +130,34 @@ applications.deploy = function*(application,ctx) {
 			ctx.body = render(inserted, null, null, -1, "部署创建失败");
 		}
 	} else {
-
+		var orderNo = _md5.md5Sign("gospel", uuid.v4());
+		var pay_url = yield pay.wechat.wxpay_pay({
+	      body: 'Gospel services', //商品描述,
+	      out_trade_no: orderNo, //商户订单号,
+	      total_fee: application.price, //金额,
+		//   total_fee: order.price * 100, //金额,
+	      spbill_create_ip: '127.0.0.1', //终端IP,
+	      product_id: '', //商品id 选,
+	      detail: 'dodora', //商品详情 选
+	      attach: '', //附加数据 选
+	      time_start: '', //订单生成时间 yyyyMMddHHmmss  选
+	      time_expire: '', //订单失效时间，格式为yyyyMMddHHmmss  选
+	      fee_type: '', //货币类型 选
+	    }, ctx);
+		var url = pay.alipay.create_direct_pay_by_user({
+		  out_trade_no: orderNo //商户订单号, 商户网站订单系统中唯一订单号，必填
+		  ,
+		  subject: 'Gospel services' //订单名称 必填
+		  ,
+		  total_fee: 0.01 //付款金额,必填
+		  ,
+		  body: "dodora" //订单描述
+		  ,
+		  show_url: '1' //商品展示地址 需以http://开头的完整路径，例如：http://www.xxx.com/myorder.html
+		}, ctx);
 		var order = yield models.gospel_orders.create({
 			products: application.products,
-			orderNo: _md5.md5Sign("gospel", uuid.v4()),
+			orderNo: orderNo,
 			name: "付费Docker",
 			price: application.price,
 			status: 1,
@@ -116,7 +166,9 @@ applications.deploy = function*(application,ctx) {
 			timeUnit: application.unit,
 			unitPrice: application.unitPrice,
 			creator: app.creator,
-			application: application.id
+			application: application.id,
+			alipay: url,
+			wechat: pay_url.code_url
 		});
 		var inserted = yield models.gospel_applications.modify({
 			id: application.id,
