f5d7efd7baa838b80370cb1e86da664b53c58e5f
SharksevenRo <937257166@qq.com>
2017-01-08 20:34:19 +0800

    update

--- controllers/applications.js
+++ controllers/applications.js
@@ -289,6 +289,13 @@ applications.create = function*() {
 	var application = yield parse(this, {
 		limit: '10kb'
 	});
+	var count = yield models.gospel_applications.count({
+		creator: application.creator
+	});
+	if(count[0].dataValues.all >= 3){
+		this.body = render(null, null, null, -1, "应用创建失败,封测期间每个用户只能创建3个应用");
+		return ;
+	}
 	if(application.id !=null && application.id != undefined && application.id != ''){
 		yield applications.deploy(application,this);
 	}else {
