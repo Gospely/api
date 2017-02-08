535a363de6ef5059ab448fe37c56b10d29daaec7
aphid-zl <lfzeng34@gmail.com>
2017-02-08 09:32:24 +0800

    update

--- shell/index.js
+++ shell/index.js
@@ -661,7 +661,7 @@ shells.clearApp = function(options){
 shells.packApp = function(options) {
     return new Promise(function(resolve, reject) {
         console.log(options);
-        var bash = 'zip -r /var/www/storage/codes/temp/' + options.appName + '.zip /var/www/storage/codes/' + options.user + '/' + options.projectFolder + '/*';
+        var bash = 'sh -c "cd /var/www/storage/codes/' + options.user + ' && zip -r /var/www/storage/codes/temp/' + options.appName + '.zip ' + options.projectFolder + '/*"';
         console.log(bash);
         exec(bash, {
             maxBuffer:  1024 * 1024
