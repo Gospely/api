9931bb771c3b43a66d9459ee12d85c7ed2e865ea
SharksevenRo <937257166@qq.com>
2017-03-07 17:22:20 +0800

    update

--- test.js
+++ test.js
@@ -1,9 +1,5 @@
 var util = require('./utils.js');
 
-var content = "src=styles.NzbWTd6Kpy.css";
-content = content.replace(/styles.\w{10}.css/, 'tst');
+var content = "height: 200%;";
+content = content.replace(/height: \d*(%|px);/,"height: " + 100);
 console.log(content);
-
-for (var i = 0; i < 9; i++) {
-    console.log(util.randomString(8, 10).length);
-}
