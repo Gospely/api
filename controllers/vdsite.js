feada5949fae645eadb16fff8ffb838d62164b64
root <937157166@qq.com>
2017-03-08 11:19:02 +0800

    updat

--- controllers/vdsite.js
+++ controllers/vdsite.js
@@ -128,7 +128,7 @@ var vdsite = {
 		console.log(app);
 		//创建文件夹，随机字符串
 
-		var randomDir = baseDir ,
+		var randomDir = baseDir + app.folder,
 			stylesName = 'styles.'+ util.randomString(8, 10) + '.css';
 			scriptsName = 'main.' + util.randomString(8, 10) + '.js';
 		delete app['folder'];
