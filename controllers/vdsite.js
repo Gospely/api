4c0328c0aa7e4232926bed617ba10aeb116231e2
SharksevenRo <937257166@qq.com>
2017-03-09 19:31:37 +0800

    update

--- controllers/vdsite.js
+++ controllers/vdsite.js
@@ -204,30 +204,28 @@ var vdsite = {
 		yield pageGenerator(this, false);
 
 		//计算打包次数
-		var app = yield parse(this);
-		if(typeof app == 'string') {
-			app = JSON.parse(app);
-		}
-		console.log(app);
-		console.log(app.folder);
-		var user = app.folder.split('/')[0];
-		var data = yield models.gospel_counts.getAll({
-			userId: user
-		});
-		console.log(data);
-
-		if (data.length !=1) {
-			var inserted = yield models.gospel_counts.create({
-				userId: user,
-				packCount: 1
-			});
-		} else {
-			var packCount = data[0].dataValues.packCount;
-			var modify = yield models.gospel_counts.modify({
-				id: data[0].dataValues.id,
-				packCount: packCount+1
-			});
-		}
+		// var app = yield parse(this);
+		// if(typeof app == 'string') {
+		// 	app = JSON.parse(app);
+		// }
+		// var user = app.folder.split('/')[0];
+		// var data = yield models.gospel_counts.getAll({
+		// 	userId: user
+		// });
+		// console.log(data);
+		//
+		// if (data.length !=1) {
+		// 	var inserted = yield models.gospel_counts.create({
+		// 		userId: user,
+		// 		packCount: 1
+		// 	});
+		// } else {
+		// 	var packCount = data[0].dataValues.packCount;
+		// 	var modify = yield models.gospel_counts.modify({
+		// 		userId: user,
+		// 		packCount: packCount+1
+		// 	});
+		// }
 	},
 
 	download: function *() {
@@ -262,12 +260,19 @@ var vdsite = {
 				userId: user,
 				downloadCount: 1
 			});
+			if(!inserted) {
+				this.throw(405, "不能被成功添加");
+			}
+			this.body = render(inserted, 1, '新增成功');
 		} else {
 			var downloadCount = data[0].dataValues.downloadCount;
 			var modify = yield models.gospel_counts.modify({
-				id: data[0].dataValues.id,
+				userId: user,
 				downloadCount: downloadCount+1
 			});
+			if(!modify) {
+				this.throw(405, "不能修改次数");
+			}
 		}
 
 	},
