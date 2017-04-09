ce9c18c3e8fec119f9d52f4986909112a3c3ac9f
Sharkseven <937257166@qq.com>
2017-04-09 13:45:31 +0800

    update

--- models/templateModel.js
+++ models/templateModel.js
@@ -66,7 +66,7 @@ module.exports = function(sequelize, DataTypes) {
       countInit: function(item) {
 
           if(item.application){
-              return 'SELECT count(a.id) as all  from gospel_templates where application=:application';
+              return 'SELECT count(id) as all  from gospel_templates where application=:application';
           }
           if(!item.type && !item.owner && !item.price){
                return 'SELECT count(a.id) as all  from gospel_templates as a left join (select * from gospel_orders where creator=:creator) as b on a.id=b.products';
