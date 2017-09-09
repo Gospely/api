155ca0b2fcb9b988b2108f19df246ab9f6b54b67
SharksevenRo <937257166@qq.com>
2017-09-09 09:10:17 +0800

    update

--- controllers/users.js
+++ controllers/users.js
@@ -232,7 +232,7 @@ users.register = function*() {
  			name: '个人版',
  			creator: inserted.id,
  			product: 'common',
-			expireat: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+			expireAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
  		});
 
  		var token = uuid.v4();
